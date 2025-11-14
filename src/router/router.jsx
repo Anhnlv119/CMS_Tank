c5fe1e3c82cee8db0fe31de6ce6d2d097993daf1
Anhnlv119 <nlevuanh@gmail.com>
2025-11-14 15:43:29 +0700

    2nd commit

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,56 +1,76 @@
 import { createBrowserRouter, redirect } from "react-router-dom"
-// import Home from "../page/ListUsers"
-// import PhotoAlbum from "../page/PhotoAlbum"
-// import UserInfo from "../page/UserInfo"
 import Login from "../page/login.jsx"
 import ListUsers from "../page/userList.jsx"
 import Leaderboard from "../page/leaderboard.jsx"
-const router = createBrowserRouter([
-    {
-        path: '/',
-        loader: () => {
-            const token = localStorage.getItem('authToken');
-            if (token) {
-                console.log(token);
-                return redirect('/home')
-            } else {
-                return redirect('/login')
-            }
-        }
-    },
-    {
-        path: '/leaderboard/:leaderboard',
-        element: <Leaderboard />,
-        loader: () => {
-            const token = localStorage.getItem('authToken');
-            if (!token) {
-                return redirect('/login')
-            }
-            return null
-        }
-    },
-    {
-        path: '/login',
-        element: <Login />,
-        loader: () => {
-            const token = localStorage.getItem('authToken');
-            if (token) {
-                return redirect('/home')
-            }
-            return null
-        }
-    },
-    {
-        path: '/home',
-        element: <ListUsers />,
-        loader: () => {
-            const token = localStorage.getItem('authToken');
-            if (!token) {
-                return redirect('/login')
-            }
-            return null
-        }
+
+const isTokenValid = () => {
+    const token = localStorage.getItem('authToken');
+    const tokenExpiration = localStorage.getItem('tokenExpiration');
+    
+    if (!token || !tokenExpiration) {
+        return false;
     }
-])
+    
+    // Check if token has expired
+    if (Date.now() > parseInt(tokenExpiration)) {
+        localStorage.removeItem('authToken');
+        localStorage.removeItem('tokenExpiration');
+        return false;
+    }
+    
+    return true;
+};
+
+const router = createBrowserRouter(
+    [
+        {
+            path: '/',
+            loader: () => {
+                if (isTokenValid()) {
+                    return redirect('/home')
+                } else {
+                    return redirect('/login')
+                }
+            }
+        },
+        {
+            path: '/leaderboard/:leaderboard',
+            element: <Leaderboard />,
+            loader: () => {
+                if (!isTokenValid()) {
+                    return redirect('/login')
+                }
+                return null
+            }
+        },
+        {
+            path: '/login',
+            element: <Login />,
+            loader: () => {
+                if (isTokenValid()) {
+                    return redirect('/home')
+                }
+                return null
+            }
+        },
+        {
+            path: '/home',
+            element: <ListUsers />,
+            loader: () => {
+                if (!isTokenValid()) {
+                    return redirect('/login')
+                }
+                return null
+            }
+        },
+        {
+            path: '*',
+            loader: () => redirect('/login')
+        }
+    ],
+    {
+        basename: import.meta.env.DEV ? '/' : '/your-repo-name'
+    }
+)
 
 export default router
